dabc1fa7f311b33aa44b57d150d312be032a13a5
Andreas <andreas.cealera@gmail.com>
2018-10-20 19:02:39 +0100

    shitloads and dash stuff

---
+++ SCMAJam/Assets/scripts/chestScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestScript : MonoBehaviour {
+
+    List<string> buffs = new List<string>();
+    public string selectedBuff;
+    bool hasBuff = true;
+
+	// Use this for initialization
+	void Start () {
+        buffs.Add("fast");
+        buffs.Add("slow");
+        buffs.Add("coins");
+
+        selectedBuff = buffs[Random.Range(0, buffs.Count-1)];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string getBuff()
+    {
+        hasBuff = false;
+        return selectedBuff;
+    }
+}
